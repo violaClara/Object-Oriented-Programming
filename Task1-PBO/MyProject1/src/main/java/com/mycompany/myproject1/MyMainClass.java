f033da28cceef88e2218d42aded38a510620b24d
Viola Clara Citra P <88891835+violaClara@users.noreply.github.com>
2022-08-26 07:33:34 +0700

    Task 1
    .jar file in target folder

---
+++ Task1-PBO/MyProject1/src/main/java/com/mycompany/myproject1/MyMainClass.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.myproject1;
+
+/**
+ *
+ * @author DELL
+ */
+public class MyMainClass {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        System.out.println("Hello Java");
+        System.out.println("Java is fun");
+        
+          
+    }
+}
---
+++ Task1-PBO/MyProject2/src/main/java/com/mycompany/myproject2/NameArt.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.myproject2;
+
+/**
+ *
+ * @author DELL
+ */
+public class NameArt {
+
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("VVV \t      VVV  III \t    OOO OOO");
+        System.out.println(" VVV \t     VVV   III \t  OOOO\t OOOO");
+        System.out.println("  VVV \t    VVV    III \t OOOO\t  OOOO");
+        System.out.println("   VVV \t   VVV \t   III \tOOOO\t   OOOO");
+        System.out.println("    VVV   VVV \t   III \t OOOO\t  OOOO");
+        System.out.println("     VVV VVV \t   III \t  OOOO\t OOOO");
+        System.out.println("      VVVVV \t   III \t    OOO OOO");
+
+    }
+}
